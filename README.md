7a13ebced896ec413364b0cdcd84c10feb6c0da3
robinp1k2 <robin@robinsit.com>
2016-01-31 11:48:47 -0600

    Add custom modifications to complete project.

--- README.md
+++ README.md
@@ -1,6 +1,46 @@
 frontend-nanodegree-arcade-game
 ===============================
+Created by:			Robin L. Paone
+Original source:  	Udacity Front-End Web Developer Nanodegree
 
-Students should use this [rubric](https://www.udacity.com/course/viewer/#!/c-nd001/l-2696458597/m-2687128535) for self-checking their submission.
+How to Install
+--------------
+Simply copy the project files onto your computer using the same directory structure.  Open "index.html" in your favorite browser to start the game.
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+How to Play
+-----------
+- The goal is to guider your player past enemies to reach a lake.  Use arrow keys to navigate your player.
+- Your player starts on the grassy lawn.  The enemies are moving along a stone path which you must cross to reach the lake.  Keep your player away from the enemies.
+- Each time you sucessfully cross the path and reach the lake you will get a point.  Press the "home" button to try again for more points.
+- If an enemy "gets" you then you will lose a point and be moved back to the lawn.
+- To win you need a total of 5 points.
+- You lose if your score goes below zero.
+- Press the spacebar to restart the game.
+
+Description
+-----------
+This is a simple JavaScript game created by drawing on the canvas.  The scene is a grassy section and water which is separated by a stone path.  The starting point for the character is in the grassy section.  Arrow keys are used to guide a character across the path to reach the water.  The users must navigate the character around enemies which randomly glide along the path.  Each time the player crosses the path without touching an enemy the score is increased by one point. If the player comes in contact with an enemy then a point is lost and the player location is reset to the starting point in the grass.  If the score goes below zero the game is lost.  If the score reaches the winning score the game is won.  After winning or losing the user may reset the game and play continuously.  Instructions to the user are displayed as the game is played.
+
+Included Files
+--------------
+README.md (this file) - software documentation
+index.html - website load page
+Folder "css":
+	style.css - Style sheet
+Folder "images" contains all imagery used in the game:
+	char-boy.png
+	enemy-bug.png
+	grass-block.png
+	stone-block.png
+	water-block.png
+	(and optionally other image files you wish to add for enhancing game.)
+Folder "js":
+	app.js - application code
+	engine.js - game looping code
+	resource.js - image loading utility
+
+Customizing
+-----------
+Modify the file "app.js" to make some easy customizations.  At the top of this file are  global variables for setting the player and enemy images. Also, the number of points required to win the game can be set.  
+
+For making even more customizations read through the code in "app.js".  The "engine.js" and other files are also a possibility for even more specialized changes.
--- js/app.js
+++ js/app.js
@@ -1,19 +1,113 @@
+// Customize your game here:
+	//Set a images for the enemy and player
+	var bugImage = 'images/enemy-bug.png';
+	var playerImage = 'images/char-boy.png'; //Could not get any other characters to work! bad file?
+	//Set the winning score
+	var winningScore = 5;
+
+// Other Global Variables
+var canvasWidth = 505;
+var canvasHeight = 606;
+var blockHeight = 83;
+var blockWidth = canvasWidth / 5;
+var pStartX = 200; // starting location of the player
+var pStartY = 405;
+var theScore = 0;
+var gameOver = false;
+
+// Work the scoreboard
+var SetScore = function(n) {
+    var scoreTextX = 0;
+    var scoreTextY = 603;
+    var scoreBoxWidth = 500;
+    var scoreBoxHeight = 20;
+    var scoreBoxX = 60;
+    var scoreBoxY = 606 - scoreBoxHeight;
+    var sayText;
+
+    theScore = theScore + n;
+
+    // clear out the old score
+    ctx.fillStyle = "#ffffff";
+    ctx.fillRect(scoreTextX, scoreBoxY, scoreBoxWidth, scoreBoxHeight);
+
+    // see if the player has won or lost (if score gets to 10 or goes below 0)
+    if (theScore >= winningScore) {
+        gameOver = true;
+        sayText = "Congratulations!  You won! (Press <space> to play again.)";
+    } else if (theScore < 0) {
+        gameOver = true;
+        sayText = "You lost!   (Press <space> to play again.)";
+    } else if (theScore === 0) {
+        sayText = "Welcome to Bugmania! Use arrow keys to play. Score of " + winningScore + " wins!";
+    }
+
+    // Update the scoreboard
+    if (gameOver || theScore === 0) {
+
+        // write "win or lose"
+        ctx.font = "12pt sans-serif";
+        ctx.textAlign = "left";
+        ctx.strokeStyle = "#808080";
+        ctx.fillStyle = "#33CC33";
+        ctx.strokeText(sayText, scoreTextX, scoreTextY);
+        ctx.fillText(sayText, scoreTextX, scoreTextY);
+
+    } else {
+
+        // write "score"
+        ctx.font = "14pt sans-serif";
+        ctx.textAlign = "left";
+        ctx.strokeStyle = "#808080";
+        ctx.fillStyle = "#33CC33";
+        ctx.strokeText("Score: ", scoreTextX, scoreTextY);
+        ctx.fillText("Score: ", scoreTextX, scoreTextY);
+
+        // write the new score
+        ctx.font = "14pt sans-serif";
+        ctx.textAlign = "left";
+        ctx.strokeStyle = "#808080";
+        ctx.fillStyle = "#000000";
+        //ctx.strokeText(theScore, scoreBoxX,scoreTextY);
+        //ctx.fillText(theScore, scoreBoxX,scoreTextY);
+        ctx.strokeText(theScore + " (Press <home> after reaching top to continue.)", scoreBoxX, scoreTextY);
+        ctx.fillText(theScore + " (Press <home> after reaching top to continue.)", scoreBoxX, scoreTextY);
+    }
+};
+
 // Enemies our player must avoid
+var randomStart = function(theObject) {
+    var bugMinX = -400;
+    var bugMaxX = -20; //-200 bugMaxX
+    theObject.x = Math.random() * (bugMaxX - bugMinX) + bugMinX;
+    var bugMinY = 60;
+    //var bugMaxY = 226;  Instead of using a range, make the bugs follow the 3 paths
+    theObject.y = bugMinY + blockHeight * (Math.round(Math.random() * 2));
+};
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    this.sprite = bugImage;
+    this.velocity = Math.random() * 200 + 75;
+    randomStart(this);
 };
-
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + (this.velocity * dt);
+
+    //if x > ?? then reset to random x and y starting position
+    if (this.x > canvasWidth) {
+        randomStart(this);
+    }
+    // see if got the player!
+    this.gotPlayer();
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,16 +115,116 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// See if enemy is touching player
+Enemy.prototype.gotPlayer = function() {
+    var spriteWidth = 83;
+    if ((((this.x + spriteWidth) > player.x) && (this.x < (player.x + spriteWidth))) &&
+        ((this.y < player.y) && (this.y > (player.y - blockHeight)))) {
+        // If touching move player back to start
+        player.x = pStartX;
+        player.y = pStartY;
+		// remove a point
+        SetScore(-1);
+    }
+};
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
 
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = playerImage;
+    this.x = pStartX; //TODO
+    this.y = pStartY;
+};
+
+Player.prototype.update = function() {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+Player.prototype.handleInput = function(theKey) {
+    var origX = this.x;
+    var origY = this.y;
+
+    if (gameOver) {
+        // Ignore all keys except "spacebar"
+        if (theKey === 'space') {
+            // Users wants to play another game. Reset.
+			doReset();
+        }
+    } else {
+        // Game is not over
+        if (this.y >= 0) {
+            //Player is not at the top.  See if any arrow keys pressed.
+            switch (theKey) {
+                case "left":
+                    this.x = this.x - blockWidth;
+                    break;
+                case "right":
+                    this.x = this.x + blockWidth;
+                    break;
+                case "up":
+                    this.y = this.y - blockHeight;
+                    break;
+                case "down":
+                    this.y = this.y + blockHeight;
+                    break;
+            }
+        } else {
+            //Player moved to the top.  Ignore all keys except "home"
+            if (theKey === 'home') {
+                // Player is at the top.  User ready to go again.  Move the player to the starting place
+                player.x = pStartX;
+                player.y = pStartY;
+            }
+        }
+    }
+
+    // See if player just now made it to the river (was not already there)
+    if ((this.y < 0) && (!gameOver) && (origY != this.y)) {
+		// Add a point
+        SetScore(1);
+    }
+    // Make sure not moved off canvas
+    if ((this.x > (canvasWidth - blockWidth)) || (this.x < (0 - blockWidth))) {
+        this.x = origX;
+    }
+    if (this.y > (canvasHeight - (1.5 * blockHeight))) {
+        this.y = origY;
+    }
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	// Initiallize scoreboard
+	SetScore(0);
+};
+
+// Game Reset
+var doReset = function() {
+	gameOver = false;
+	player.x = pStartX;
+	player.y = pStartY;
+	theScore = 0;
+	SetScore(0);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var player = new Player();
+var enemy1 = new Enemy();
+var enemy2 = new Enemy();
+var enemy3 = new Enemy();
+var enemy4 = new Enemy();
+var enemy5 = new Enemy();
+var allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -39,8 +233,9 @@ document.addEventListener('keyup', function(e) {
         37: 'left',
         38: 'up',
         39: 'right',
-        40: 'down'
+        40: 'down',
+        36: 'home',
+        32: 'space'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });

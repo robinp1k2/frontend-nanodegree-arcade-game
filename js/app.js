644a098d7bdcde24379fe8b94745dfcef6196b2d
robinp1k2 <robin@robinsit.com>
2016-02-01 20:56:53 -0600

    Update to make more object-oriented (per help from Udacity coach!)

--- js/app.js
+++ js/app.js
@@ -1,45 +1,53 @@
 // Customize your game here:
 	//Set a images for the enemy and player
-	var bugImage = 'images/enemy-bug.png';
-	var playerImage = 'images/char-boy.png'; //Could not get any other characters to work! bad file?
+	var ENEMY_IMAGE = 'images/enemy-bug.png';
+	var PLAYER_IMAGE = 'images/char-boy.png'; //Could not get any other characters to work! bad file?
+	var PLAYER_WIDTH = 83;
 	//Set the winning score
-	var winningScore = 5;
+	var WINNING_SCORE = 5;
 
-// Other Global Variables
-var canvasWidth = 505;
-var canvasHeight = 606;
-var blockHeight = 83;
-var blockWidth = canvasWidth / 5;
-var pStartX = 200; // starting location of the player
-var pStartY = 405;
-var theScore = 0;
+// Other Global Constants
+var CANVAS_WIDTH = 505;
+var CANVAS_HEIGHT = 606;
+var BLOCK_HEIGHT = 83;
+var BLOCK_WIDTH = CANVAS_WIDTH / 5;
+var PLAYER_START_X = 200; // starting location of the player
+var PLAYER_START_Y = 405;
+var SCORE_TEXT_X = 0;
+var SCORE_TEXT_Y = 603;
+var SCORE_BOX_WIDTH = 500;
+var SCORE_BOX_HEIGHT = 20;
+var SCORE_BOX_X = 60;
+var SCORE_BOX_Y = 606 - SCORE_BOX_HEIGHT;
+var ENEMY_MIN_X = -400;
+var ENEMY_MAX_X = -20;
+var ENEMY_MIN_Y = 60;
+var ENEMY_MIN_VELOCITY = 75;
+var ENEMY_MAX_VELOCITY = 200;
+
+// Global Variables
 var gameOver = false;
+var theScore = 0;
 
 // Work the scoreboard
-var SetScore = function(n) {
-    var scoreTextX = 0;
-    var scoreTextY = 603;
-    var scoreBoxWidth = 500;
-    var scoreBoxHeight = 20;
-    var scoreBoxX = 60;
-    var scoreBoxY = 606 - scoreBoxHeight;
+var setScore = function(n) {
     var sayText;
 
     theScore = theScore + n;
 
     // clear out the old score
     ctx.fillStyle = "#ffffff";
-    ctx.fillRect(scoreTextX, scoreBoxY, scoreBoxWidth, scoreBoxHeight);
+    ctx.fillRect(SCORE_TEXT_X, SCORE_BOX_Y, SCORE_BOX_WIDTH, SCORE_BOX_HEIGHT);
 
     // see if the player has won or lost (if score gets to 10 or goes below 0)
-    if (theScore >= winningScore) {
+    if (theScore >= WINNING_SCORE) {
         gameOver = true;
         sayText = "Congratulations!  You won! (Press <space> to play again.)";
     } else if (theScore < 0) {
         gameOver = true;
         sayText = "You lost!   (Press <space> to play again.)";
     } else if (theScore === 0) {
-        sayText = "Welcome to Bugmania! Use arrow keys to play. Score of " + winningScore + " wins!";
+        sayText = "Welcome to Bugmania! Use arrow keys to play. Score of " + WINNING_SCORE + " wins!";
     }
 
     // Update the scoreboard
@@ -50,8 +58,8 @@ var SetScore = function(n) {
         ctx.textAlign = "left";
         ctx.strokeStyle = "#808080";
         ctx.fillStyle = "#33CC33";
-        ctx.strokeText(sayText, scoreTextX, scoreTextY);
-        ctx.fillText(sayText, scoreTextX, scoreTextY);
+        ctx.strokeText(sayText, SCORE_TEXT_X, SCORE_TEXT_Y);
+        ctx.fillText(sayText, SCORE_TEXT_X, SCORE_TEXT_Y);
 
     } else {
 
@@ -60,40 +68,36 @@ var SetScore = function(n) {
         ctx.textAlign = "left";
         ctx.strokeStyle = "#808080";
         ctx.fillStyle = "#33CC33";
-        ctx.strokeText("Score: ", scoreTextX, scoreTextY);
-        ctx.fillText("Score: ", scoreTextX, scoreTextY);
+        ctx.strokeText("Score: ", SCORE_TEXT_X, SCORE_TEXT_Y);
+        ctx.fillText("Score: ", SCORE_TEXT_X, SCORE_TEXT_Y);
 
         // write the new score
         ctx.font = "14pt sans-serif";
         ctx.textAlign = "left";
         ctx.strokeStyle = "#808080";
         ctx.fillStyle = "#000000";
-        //ctx.strokeText(theScore, scoreBoxX,scoreTextY);
-        //ctx.fillText(theScore, scoreBoxX,scoreTextY);
-        ctx.strokeText(theScore + " (Press <home> after reaching top to continue.)", scoreBoxX, scoreTextY);
-        ctx.fillText(theScore + " (Press <home> after reaching top to continue.)", scoreBoxX, scoreTextY);
+        ctx.strokeText(theScore + " (Press <home> after reaching top to continue.)", SCORE_BOX_X, SCORE_TEXT_Y);
+        ctx.fillText(theScore + " (Press <home> after reaching top to continue.)", SCORE_BOX_X, SCORE_TEXT_Y);
     }
 };
 
 // Enemies our player must avoid
-var randomStart = function(theObject) {
-    var bugMinX = -400;
-    var bugMaxX = -20; //-200 bugMaxX
-    theObject.x = Math.random() * (bugMaxX - bugMinX) + bugMinX;
-    var bugMinY = 60;
-    //var bugMaxY = 226;  Instead of using a range, make the bugs follow the 3 paths
-    theObject.y = bugMinY + blockHeight * (Math.round(Math.random() * 2));
-};
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = bugImage;
-    this.velocity = Math.random() * 200 + 75;
-    randomStart(this);
+    this.sprite = ENEMY_IMAGE;
+    this.velocity = (Math.random() * ENEMY_MAX_VELOCITY) + ENEMY_MIN_VELOCITY;
+	this.randomStart();
 };
+
+Enemy.prototype.randomStart = function() {
+   this.x = (Math.random() * (ENEMY_MAX_X - ENEMY_MIN_X)) + ENEMY_MIN_X;
+   this.y = ENEMY_MIN_Y + (BLOCK_HEIGHT * (Math.round(Math.random() * 2)));
+};
+
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
@@ -103,8 +107,8 @@ Enemy.prototype.update = function(dt) {
     this.x = this.x + (this.velocity * dt);
 
     //if x > ?? then reset to random x and y starting position
-    if (this.x > canvasWidth) {
-        randomStart(this);
+    if (this.x > CANVAS_WIDTH) {
+        this.randomStart();
     }
     // see if got the player!
     this.gotPlayer();
@@ -117,14 +121,12 @@ Enemy.prototype.render = function() {
 
 // See if enemy is touching player
 Enemy.prototype.gotPlayer = function() {
-    var spriteWidth = 83;
-    if ((((this.x + spriteWidth) > player.x) && (this.x < (player.x + spriteWidth))) &&
-        ((this.y < player.y) && (this.y > (player.y - blockHeight)))) {
+    if ((((this.x + PLAYER_WIDTH) > player.x) && (this.x < (player.x + PLAYER_WIDTH))) &&
+        ((this.y < player.y) && (this.y > (player.y - BLOCK_HEIGHT)))) {
         // If touching move player back to start
-        player.x = pStartX;
-        player.y = pStartY;
+		player.resetPosition();
 		// remove a point
-        SetScore(-1);
+        setScore(-1);
     }
 };
 
@@ -137,9 +139,8 @@ var Player = function() {
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = playerImage;
-    this.x = pStartX; //TODO
-    this.y = pStartY;
+    this.sprite = PLAYER_IMAGE;
+	this.resetPosition();
 };
 
 Player.prototype.update = function() {
@@ -156,7 +157,7 @@ Player.prototype.handleInput = function(theKey) {
         // Ignore all keys except "spacebar"
         if (theKey === 'space') {
             // Users wants to play another game. Reset.
-			doReset();
+			this.newGame();
         }
     } else {
         // Game is not over
@@ -164,24 +165,23 @@ Player.prototype.handleInput = function(theKey) {
             //Player is not at the top.  See if any arrow keys pressed.
             switch (theKey) {
                 case "left":
-                    this.x = this.x - blockWidth;
+                    this.x = this.x - BLOCK_WIDTH;
                     break;
                 case "right":
-                    this.x = this.x + blockWidth;
+                    this.x = this.x + BLOCK_WIDTH;
                     break;
                 case "up":
-                    this.y = this.y - blockHeight;
+                    this.y = this.y - BLOCK_HEIGHT;
                     break;
                 case "down":
-                    this.y = this.y + blockHeight;
+                    this.y = this.y + BLOCK_HEIGHT;
                     break;
             }
         } else {
             //Player moved to the top.  Ignore all keys except "home"
             if (theKey === 'home') {
                 // Player is at the top.  User ready to go again.  Move the player to the starting place
-                player.x = pStartX;
-                player.y = pStartY;
+				this.resetPosition();
             }
         }
     }
@@ -189,42 +189,44 @@ Player.prototype.handleInput = function(theKey) {
     // See if player just now made it to the river (was not already there)
     if ((this.y < 0) && (!gameOver) && (origY != this.y)) {
 		// Add a point
-        SetScore(1);
+        setScore(1);
     }
     // Make sure not moved off canvas
-    if ((this.x > (canvasWidth - blockWidth)) || (this.x < (0 - blockWidth))) {
+    if ((this.x > (CANVAS_WIDTH - BLOCK_WIDTH)) || (this.x < (0 - BLOCK_WIDTH))) {
         this.x = origX;
     }
-    if (this.y > (canvasHeight - (1.5 * blockHeight))) {
+    if (this.y > (CANVAS_HEIGHT - (1.5 * BLOCK_HEIGHT))) {
         this.y = origY;
     }
 };
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-	// Initiallize scoreboard
-	SetScore(0);
+	// Initialize scoreboard
+	setScore(0);
 };
 
+Player.prototype.resetPosition = function() {
+	this.x = PLAYER_START_X;
+	this.y = PLAYER_START_Y;	
+}
+
 // Game Reset
-var doReset = function() {
+Player.prototype.newGame = function() {
 	gameOver = false;
-	player.x = pStartX;
-	player.y = pStartY;
+	this.resetPosition();
 	theScore = 0;
-	SetScore(0);
+	setScore(0);
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var allEnemies = [];
+for (var i = 0; i < 5; i++) {
+	allEnemies.push(new Enemy());
+}
 // Place the player object in a variable called player
 var player = new Player();
-var enemy1 = new Enemy();
-var enemy2 = new Enemy();
-var enemy3 = new Enemy();
-var enemy4 = new Enemy();
-var enemy5 = new Enemy();
-var allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
